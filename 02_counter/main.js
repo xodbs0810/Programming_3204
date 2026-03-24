aaa5cb16ac54a720e6d9a1fcc411c8bb6a169ee3
3204박태윤 <qaz080725@kakao.com>
2026-03-24 09:46:15 +0900

    counter HTML id, class JS addEventLisetener(), onclick

---
+++ 02_counter/main.js
@@ -0,0 +1,21 @@
+let count = 0;
+// HTML -> JS 
+// const resultH1 = document.querySelectorAll('h1')[0];
+// const resultH1 = document.querySelector('h1');
+const resultH1 = document.getElementById('result'); 
+// const resultH1 = document.querySelector('#result');
+// const resultH1 = resultH1;
+const plusButton = document.getElementsByClassName("plusBtn")[0];
+
+plusButton.onclick = () => { 
+    // 숫자 증가 
+count++; //count += 1; // count = count + 1;
+
+// 숫자 표시 
+resultH1.innerText = count; 
+};
+
+// (함수정의)();
+// (function a(){
+    
+// })()
