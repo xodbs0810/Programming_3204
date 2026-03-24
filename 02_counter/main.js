c1b3b88781266932d6ef81d842b5502a00890fdb
3204박태윤 <qaz080725@kakao.com>
2026-03-24 09:52:36 +0900

    counter HTML onclick

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -7,6 +7,16 @@ const resultH1 = document.getElementById('result');
 // const resultH1 = resultH1;
 const plusButton = document.getElementsByClassName("plusBtn")[0];
 
+// plusButton.addEventListener('click', () => {
+//     // 숫자 증가 
+// count++; //count += 1; // count = count + 1;
+
+//     // 숫자 표시 
+// resultH1.innerText = count; 
+// });
+function plus(){
+    // 숫자 증가
+    
 plusButton.onclick = () => { 
     // 숫자 증가 
 count++; //count += 1; // count = count + 1;
@@ -14,6 +24,9 @@ count++; //count += 1; // count = count + 1;
 // 숫자 표시 
 resultH1.innerText = count; 
 };
+
+    
+}
 
 // (함수정의)();
 // (function a(){
