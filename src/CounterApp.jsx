3919b60f9f36c0c863968f942f15991d7e0b21b2
3204박태윤 <qaz080725@kakao.com>
2026-03-31 09:38:20 +0900

    CounterApp +2, -1

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -11,7 +11,10 @@ function CounterApp() {
   return (
     <> 
     <h1>{count}</h1> 
-    <button  onClick={() => setCount((count) => count + 1)}>+</button>      
+    <button  onClick={() => setCount((count) => count + 1)}>+</button>  
+    <button  onClick={() => setCount((count) => count + 2)}>+2</button>
+    <button  onClick={() => setCount((count) => count - 1)}>-</button>
+    
     {/* <button
           className="counter"
           onClick={() => setCount((count) => count + 1)} // JS 함수 호출 X, 함수 정의 O 
