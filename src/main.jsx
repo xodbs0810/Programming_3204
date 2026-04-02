f55e0c11d430814e34016c4c2251a012aec14c03
3204박태윤 <qaz080725@kakao.com>
2026-04-02 14:27:11 +0900

    TodoListApp reset.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
+import './reset.css'
 import TodoListApp from './TodoListApp.jsx'
 
 createRoot(document.getElementById('root')).render(
