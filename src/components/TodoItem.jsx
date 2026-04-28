3f707ae0a584555a2397635929d3f35669afdf24
3204박태윤 <qaz080725@kakao.com>
2026-04-28 09:04:05 +0900

    TodoListApp LocalStorage

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,30 @@
 import Checkbox from "./Checkbox.jsx";
 import Button from "./Button.jsx";
 
-export default function TodoItem( {todo} ){
+export default function TodoItem( {todo, toggleTodo, deleteTodo, editTodo} ){ 
+    function handleEditClick(){
+        if(!is)
+    }
+    type="text"
+    className="todo__input--edit"
+    value={editTodo}
     return(
         <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
-          <Checkbox className="todo__check" id={todo.id} > {todo.text} </Checkbox>
+            {!isEditing &&
+                <checkbox
+                    id={todo.id}
+                    checked={todo.isCompleted}
+                    onChange={() => toggleTodo(todo.id)}
+                    >{todo.text}</checkbox>
+            </checkbox>
+            }
+          <Checkbox className="todo__check" id={todo.id} checked={todo.isCompleted} onChange={() => toggleTodo(todo.id)}>
+            {todo.text}
+          </Checkbox>
           <Button className="todo__button todo__button--edit">🅾️</Button>
           <Button 
           className="todo__button todo__button--delete"
-          onClick={() => deleteTodo()}
+          onClick={() => deleteTodo(todo.id)}
           >❌</Button>
       </li>
     )
