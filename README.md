dc5264f64f55aecb473821278e16b26f6778285b
3204박태윤 <qaz080725@kakao.com>
2026-04-28 09:26:09 +0900

    TodoListApp

--- README.md
+++ README.md
@@ -9,4 +9,19 @@ npm create vite@latest .
 - useState()
 - onClick={() => set 함수((state변수) => state변수 +1)}
 - onClick ={함수이름}
-### TodoListApp 🌸
+### TodoListApp 🌸 
+- React Component 분리 
+- for -> htmlFor, class -> className 
+- props
+- `<input id={id} value={}/>`, `<label htmlFor={id} /> `
+- onchange
+- 구조 분해 할당, `...스프레드연산자`
+- `<form onSubmit={}></form>`
+- `map()`:RU
+- `filter()` : D
+- `<TodoItem key{}/>`
+- `const handleEvent = (event) => {} `
+- `{조건식 ? 참 : 거짓 }`
+- `{조건식 && 참}`, `{!조건식 && 거짓}`
+- onKeyDown
+- LocalStorage, `useEffect()`
--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -16,9 +16,22 @@ class Todo {
     this.text = text;
     this.isCompleted = isCompleted;
   }
+
 }
 function TodoListApp() {
-  const [todos, setTodos] = useState([]);
+  const initTodos = () => {
+    //localstorage에서 가져오기 
+    const savedTodos = localStorage.getItem(TODOS_STORAGE_KEY);
+    //값이 없으면 [] 
+    //값이 있으면 todos의 초기값 대입
+    return (!savedTodos) ? [] : savedTodos;
+  }
+  const [todos, setTodos] = useState(initTodos());
+  //Todos 변경 시, localStorage에 todos 저장 
+  useEffect(() => {
+    localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));//JSON 객체 또는 리스트 -> string
+  }, [todos]);
+
   function addTodo(text) {
     //이전 todos에 newTodo 만들어서 추가 -> 그것을 setTodos() 하자 
     setTodos((todos) => [
@@ -40,9 +53,17 @@ function TodoListApp() {
   )
 }
 function deleteTodo(id){
-  //todos를 하나씩 꺼내어 todo, 
-  setTodos((todos) => 
-  
+  todos.filter((todo) => todo.id !== id));
+}
+function editTodo(id, newText){
+  setTodos((todos) =>
   )
 }
+return (
+  <div className="todo">
+    <TodoHeader />
+    <TodoAdder addTodo={addTodo}/>
+    
+  </div>
+)
 export default TodoListApp; 
